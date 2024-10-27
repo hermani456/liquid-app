48ca15e9041e45bd99b2ddcd6e3f12e20c0c59c7
hermani <hermani457@gmail.com>
2024-10-27 20:05:36 -0300

    Refactor PDF template to include employee information and details

--- src/components/pdf/Template.jsx
+++ src/components/pdf/Template.jsx
@@ -34,6 +34,61 @@ const page = () => {
             <h2 className="text-xl">Liquidación de sueldo</h2>
             <h3 className="font-normal">Octubre 2024</h3>
           </div>
+          <div
+            id="employee-data"
+            className="border rounded-md text-sm overflow-hidden mt-3"
+          >
+            <div className="bg-black/20 p-1">
+              <h2 className="font-bold">Información del trabajador</h2>
+            </div>
+            <div className="flex justify-between p-1">
+              <div className="flex flex-col">
+                <h6 className="font-semibold">
+                  Nombre: <span className="font-normal">Emilio Acevedo</span>
+                </h6>
+                <h6 className="font-semibold">
+                  Rut: <span className="font-normal">12.345.678-9</span>
+                </h6>
+                <h6 className="font-semibold">
+                  Cargo: <span className="font-normal">Desarrollador</span>
+                </h6>
+              </div>
+              <div className="flex flex-col">
+                <h6 className="font-semibold">
+                  Trabaja desde: <span className="font-normal">01/01/2020</span>
+                </h6>
+                <h6 className="font-semibold">
+                  Contrato: <span className="font-normal">Indefinido</span>
+                </h6>
+                <h6 className="font-semibold">
+                  Email: <span className="font-normal">emilio@a7.cl</span>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div
+            id="details"
+            className="border rounded-md text-sm overflow-hidden mt-3"
+          >
+            <div className="bg-black/20 p-1">
+              <h2 className="font-bold">Detalle</h2>
+            </div>
+            <div className="flex divide-x-2 justify-between p-1">
+              <div className="flex space-x-5">
+                <div>Dias trabajados: 30</div>
+                <div>Feriados: 2</div>
+                <div>Ausentes: 0</div>
+              </div>
+              <div className="flex space-x-5">
+                <div>AFP: UNO</div>
+                <div>Tasa: 10.49%</div>
+              </div>
+              <div className="flex space-x-5">
+                <div>Salud: Banmedica</div>
+                <div>Cargas: 0</div>
+              </div>
+            </div>
+          </div>
         </div>
       </Tailwind>
     </div>
