6e08d6437310a15ef242b97fdba681692bfe58f6
hermani456 <hermani457@gmail.com>
2024-11-02 19:14:56 -0300

    Add ThemeSwitcher component and integrate it into the dashboard layout

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarLayout, SidebarTrigger } from "@/components/ui/sidebar";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 export default async function Page({ children }) {
   const { cookies } = await import("next/headers");
@@ -10,7 +11,12 @@ export default async function Page({ children }) {
       <AppSidebar />
       <main className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
         <div className="h-full rounded-md border-2 p-2">
-          <SidebarTrigger />
+          <div className="flex">
+            <SidebarTrigger />
+            <div className="ml-auto">
+              <ThemeSwitcher />
+            </div>
+          </div>
           <div className="min-h-[32rem]">{children}</div>
         </div>
       </main>
