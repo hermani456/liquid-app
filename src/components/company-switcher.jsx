8b82c4eb772afbf3d7b8bbd6fc2a34742a36f40f
hermani456 <hermani457@gmail.com>
2024-11-03 17:55:05 -0300

    Refactor CompanySwitcher component to improve state management and localStorage integration

--- src/components/company-switcher.jsx
+++ src/components/company-switcher.jsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ChevronsUpDown, Plus } from "lucide-react"
+import { useEffect, useState } from "react";
+import { ChevronsUpDown, Plus } from "lucide-react";
 
 import {
   DropdownMenu,
@@ -11,10 +11,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import { useQuery } from "@tanstack/react-query";
 import { fetchCompanies } from "@/utils/fetchFuntions";
-
 
 export function CompanySwitcher() {
   const {
@@ -27,12 +26,21 @@ export function CompanySwitcher() {
     staleTime: Infinity,
   });
 
+  // save to localStorage
+  const [activeCompany, setActiveCompany] = useState(() => {
+    const storedCompany = localStorage.getItem("activeCompany");
+    return storedCompany ? JSON.parse(storedCompany) : [];
+  });
 
-  const [activeCompany, setActiveCompany] = React.useState([])
+  useEffect(() => {
+    if (activeCompany) {
+      localStorage.setItem("activeCompany", JSON.stringify(activeCompany));
+    }
+  }, [activeCompany]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!isPending && companies) {
-      console.log("companies", companies)
+      console.log("companies", companies);
       setActiveCompany(companies[0]);
     }
   }, [companies]);
@@ -41,13 +49,10 @@ export function CompanySwitcher() {
   if (isError) return <div>Error loading companies</div>;
 
   return (
-    (<DropdownMenu>
-      <DropdownMenuTrigger
-        className="w-full rounded-md ring-ring hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 data-[state=open]:bg-accent">
-        <div
-          className="flex items-center gap-1.5 overflow-hidden px-2 py-1.5 text-left text-sm transition-all">
-          <div
-            className="flex h-5 w-5 items-center justify-center rounded-sm bg-primary text-primary-foreground">
+    <DropdownMenu>
+      <DropdownMenuTrigger className="w-full rounded-md ring-ring hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 data-[state=open]:bg-accent">
+        <div className="flex items-center gap-1.5 overflow-hidden px-2 py-1.5 text-left text-sm transition-all">
+          <div className="flex h-5 w-5 items-center justify-center rounded-sm bg-primary text-primary-foreground">
             {/* <activeTeam.logo className="h-3.5 w-3.5 shrink-0" /> */}
           </div>
           <div className="line-clamp-1 flex-1 pr-2 font-medium">
@@ -56,7 +61,12 @@ export function CompanySwitcher() {
           <ChevronsUpDown className="ml-auto h-4 w-4 text-muted-foreground/50" />
         </div>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-64" align="start" side="right" sideOffset={4}>
+      <DropdownMenuContent
+        className="w-64"
+        align="start"
+        side="right"
+        sideOffset={4}
+      >
         <DropdownMenuLabel className="text-xs text-muted-foreground">
           Empresas
         </DropdownMenuLabel>
@@ -64,9 +74,9 @@ export function CompanySwitcher() {
           <DropdownMenuItem
             key={company.name}
             onClick={() => setActiveCompany(company)}
-            className="items-start gap-2 px-1.5">
-            <div
-              className="flex h-8 w-8 items-center justify-center rounded-sm bg-primary text-primary-foreground">
+            className="items-start gap-2 px-1.5"
+          >
+            <div className="flex h-8 w-8 items-center justify-center rounded-sm bg-primary text-primary-foreground">
               {/* <team.logo className="h-5 w-5 shrink-0" /> */}
             </div>
             <div className="grid flex-1 leading-tight">
@@ -82,13 +92,14 @@ export function CompanySwitcher() {
         ))}
         <DropdownMenuSeparator />
         <DropdownMenuItem className="gap-2 px-1.5">
-          <div
-            className="flex h-8 w-8 items-center justify-center rounded-md border bg-background">
+          <div className="flex h-8 w-8 items-center justify-center rounded-md border bg-background">
             <Plus className="h-5 w-5" />
           </div>
-          <div className="font-medium text-muted-foreground">Agregar Empresa</div>
+          <div className="font-medium text-muted-foreground">
+            Agregar Empresa
+          </div>
         </DropdownMenuItem>
       </DropdownMenuContent>
-    </DropdownMenu>)
+    </DropdownMenu>
   );
 }
