cc5e493b2fba1a436871d1195ccfc5d4430270d7
hermani456 <hermani457@gmail.com>
2024-11-15 00:59:51 -0300

    Refactor landing page components: integrate Container in Hero, clean up styles, and improve layout structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import  NavBar from "@/components/landing/navbar"
-import Hero from '@/components/landing/hero'
+import NavBar from "@/components/landing/navbar";
+import Hero from "@/components/landing/hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative overflow-hidden">
       <NavBar />
       <Hero />
       <div className="flex justify-center">
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
--- src/components/landing/container.jsx
+++ src/components/landing/container.jsx
@@ -1,7 +1,5 @@
-const Container = ({children}) => {
-  return (
-    <div className="max-w-screen-xl mx-auto p-5">{children}</div>
-  )
-}
+const Container = ({ children }) => {
+  return <div className="max-w-screen-xl mx-auto p-5">{children}</div>;
+};
 
-export default Container
+export default Container;
