a72f7c9fd7cd9b5ec2ce592c54e2772ad11f9aeb
hermani <hermani457@gmail.com>
2024-12-03 01:31:22 -0300

    Add SSL configuration to PostgreSQL connection pool

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,12 @@
 import { Pool } from 'pg';
+import fs from 'fs';
 
 const pool = new Pool({
    connectionString: process.env.POSTGRES_URL,
+   ssl: {
+      rejectUnauthorized: true,
+      ca: process.env.DB_SSL_CA
+   },
 });
 // const pool = new Pool({
 //    user: process.env.PGUSER,
