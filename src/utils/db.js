3846c780ceae9e1c96427fce1d2854341dd1928e
hermani <hermani457@gmail.com>
2024-12-03 01:51:46 -0300

    Refactor PostgreSQL connection pool configuration by removing SSL settings

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,7 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 const pool = new Pool({
-   connectionString: process.env.POSTGRES_URL,
-   ssl: {
-      rejectUnauthorized: true,
-      ca: process.env.DB_SSL_CA
-   },
+  connectionString: process.env.POSTGRES_URL,
 });
 // const pool = new Pool({
 //    user: process.env.PGUSER,
